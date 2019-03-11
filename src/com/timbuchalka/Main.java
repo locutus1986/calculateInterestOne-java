88290f16e48481689becdb31051f3a3edd8bfb19
locutus1986 <locutus1986@localhost.localdomain>
2019-03-11 17:58:50 -0400

    challenged to make the loop work backwards

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,7 +3,7 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	    for(int i = 2; i < 9; i++){
+	    for(int i = 8; i >= 2; i--){
             System.out.println(
                     "10,000 at " + i  + "% interest = " +
                     String.format("%.2f", calculateInterest(10000, i)));
