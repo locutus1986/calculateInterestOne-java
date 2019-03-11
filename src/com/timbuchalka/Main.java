411afd7f86b7d632698b9171c5ea81bb47089e39
locutus1986 <locutus1986@localhost.localdomain>
2019-03-11 17:47:51 -0400

    first

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,13 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    for(int i = 2; i < 8; i++){
+            System.out.println(calculateInterest(10000, i));
+        }
+    }
+    public static double calculateInterest(double amount, double interestRate){
+        return (amount*(interestRate/100));
+    }
+}
