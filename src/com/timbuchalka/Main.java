32f8d7e117a94c2d158d1087b260e6ab25271ada
locutus1986 <locutus1986@localhost.localdomain>
2019-03-11 17:49:22 -0400

    second

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,7 +3,7 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	    for(int i = 2; i < 8; i++){
+	    for(int i = 2; i < 9; i++){
             System.out.println(calculateInterest(10000, i));
         }
     }
